444e98187561e9bebb36ef28b8dcd9687ac3df6c
Nidhi Shukla <43289190+Nidhishukla1709@users.noreply.github.com>
2022-03-11 13:52:24 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("nidhi.shukla@atmecs.com");
+                mail.To.Add("harshini.venkatesan@atmecs.com");
+                mail.Subject = "Test Mail - 1";
+                mail.Body = "mail with attachment";
+
+               // System.Net.Mail.Attachment attachment;
+               // attachment = new System.Net.Mail.Attachment("your attachment file");
+                //mail.Attachments.Add(attachment);
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("nidhi.shukla@atmecs.com", "Shukla@17091997");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("mail Send");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+}
